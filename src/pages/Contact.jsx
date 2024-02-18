474f3fa402993f36d34fe2e4e99fd6b7fa45e393
unknown <rislammb@gmail.com>
2024-02-18 13:59:10 +0600

    update minimum container height

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
   const handleBlur = () => setCurrentAnimation('idle');
 
   return (
-    <section className='relative flex lg:flex-row flex-col max-container h-[100vh]'>
+    <section className='relative flex lg:flex-row flex-col max-container'>
       {alert.show && <Alert {...alert} />}
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
